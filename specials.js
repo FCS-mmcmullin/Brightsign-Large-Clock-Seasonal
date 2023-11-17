725e523e136edc44485c6f38df2d449db5236667
FCS-mmcmullin <122109664+FCS-mmcmullin@users.noreply.github.com>
2023-11-17 10:23:15 -0500

    BGs and Thanksgiving
    Brightened backgrounds and added thanksgiving

--- specials.js
+++ specials.js
@@ -8,6 +8,13 @@ var specials =
         "video": "halloween.mp4"
     },
     {
+        "title": "Thanksgiving",
+        "start":"11/17/2023",
+        "end":"11/24/2023",
+        "background": "",
+        "video": "thanksgiving.mp4"
+    },
+    {
         "title": "Test Yesterday",
         "start":"10/18/2023",
         "end":"10/18/2023",
