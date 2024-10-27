cd92695cc1d13f1e27cb26f1103065bfc471cc8f
FCS-mmcmullin <122109664+FCS-mmcmullin@users.noreply.github.com>
2024-10-27 08:18:03 -0400

    Update specials.js
    Update dates for 2024

--- specials.js
+++ specials.js
@@ -2,20 +2,20 @@ var specials =
 [
     {
         "title": "Halloween",
-        "start":"10/19/2023",
-        "end":"10/31/2023",
+        "start":"10/27/2024",
+        "end":"10/31/2024",
         "background": "",
         "video": "halloween.mp4"
     },
     {
         "title": "Thanksgiving",
-        "start":"11/17/2023",
-        "end":"11/24/2023",
+        "start":"11/18/2024",
+        "end":"11/29/2024",
         "background": "",
         "video": "thanksgiving.mp4"
     },
     {
-        "title": "Test Yesterday",
+        "title": "Test event in past",
         "start":"10/18/2023",
         "end":"10/18/2023",
         "background": "test.png",
